6ff808272807b2b3dc0694d371febf81a54bcdeb
ok2fly <2899109958@qq.com>
2024-10-30 22:14:18 +0800

    feat: ✨ finish learn

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,4 +1,10 @@
 import SideNav from "@/app/ui/dashboard/sidenav"
+// import { Metadata } from "next"
+export const experimental_ppr = true
+
+// export const metadata: Metadata = {
+// 	title: "Invoices | Acme Dashboard",
+// }
 
 export default function Layout({ children }: { children: React.ReactNode }) {
 	return (
